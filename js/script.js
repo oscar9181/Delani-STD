60b977655b0d450b1cb0a507cafda7e2a64fdde3
oscar9181 <oscarokola17@gmail.com>
2022-03-17 17:53:21 +0300

    add index.html

--- js/script.js
+++ js/script.js
@@ -1,37 +1,27 @@
-$(document).ready(function() {
-    $('.hide').hide();
 
-    $('.hide,#design').on('click',function(){
+$(".col").click(function(){
+    $(this).children(".hide").slideToggle()
 
-        $('.hide,#design').toggle()
-    });
+});
+
     
-
-    $('.hide').hide();
-
-    $('.hide,#product').on('click',function(){
-
-        $('.hide,#product').toggle()
-    });
-
-   
-    $('.hide').hide();
-
-    $('.hide,#development').on('click',function(){
-
-        $('.hide,#development').toggle()
-    });
     $(".work").mousemove(function(){
+        
         var hover=$(this).attr("hover")
-        $(this).css("border","3px solid red")
-        $("#overlay").text(hover).show().offset($(this).offset());
-    
+        $(this).css("border","3px solid yellow")
+        $("text").css("display" , "show")
+        $(".overlay").text(hover).show().offset($(this).offset());
+
+
     
 
 }).mouseout (function(){
     $(this).css("border","none")
+    $(this).css("text","")
 })
 
 
 
-})
+
+
+
